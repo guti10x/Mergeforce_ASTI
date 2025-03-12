972ea6c7cf737fb306b7e7dab16db9597f92d120
guti10x <gt104515@gmail.com>
2025-03-12 13:40:56 +0100

    Add segundo motor + code para ir front, back, left, right

--- Raspberry/Pruebas sensores/motoresDC.py
+++ Raspberry/Pruebas sensores/motoresDC.py
@@ -1,46 +1,64 @@
 from pymata4 import pymata4
 import time
 
-# Inicializar la placa Ardls
-
+# Inicializar la placa Arduino
 board = pymata4.Pymata4()
 
 # Definir pines
 led_pin = 13
-in1 = 6 # Entrada del puente H
-in2 = 7 # Entrada del puente H
+in1 = 6  # Entrada del primer motor
+in2 = 7  # Entrada del primer motor
+in3 = 3  # Entrada del segundo motor
+in4 = 4  # Entrada del segundo motor
 
 # Configurar pines
 board.set_pin_mode_digital_output(led_pin)
 board.set_pin_mode_digital_output(in1)
 board.set_pin_mode_digital_output(in2)
+board.set_pin_mode_digital_output(in3)
+board.set_pin_mode_digital_output(in4)
 
 try:
     while True:
         # Encender LED
         board.digital_write(led_pin, 1)
         
-        # Avanzar motor por 1 segundo
+        # Ir recto (Ambos motores hacia adelante)
         board.digital_write(in1, 1)
         board.digital_write(in2, 0)
-        time.sleep(1)
+        board.digital_write(in3, 1)
+        board.digital_write(in4, 0)
+        time.sleep(2)
         
-        # Detener motor por 1 segundo
-        board.digital_write(in1, 0)
-        board.digital_write(in2, 0)
-        time.sleep(1)
-        
-        # Marcha atrás por 1 segundo
+        # Ir hacia atrás (Ambos motores hacia atrás)
         board.digital_write(in1, 0)
         board.digital_write(in2, 1)
-        time.sleep(1)
+        board.digital_write(in3, 0)
+        board.digital_write(in4, 1)
+        time.sleep(2)
+        
+        # Girar a la derecha (Motor 1 adelante, Motor 2 atrás)
+        board.digital_write(in1, 1)
+        board.digital_write(in2, 0)
+        board.digital_write(in3, 0)
+        board.digital_write(in4, 1)
+        time.sleep(2)
+        
+        # Girar a la izquierda (Motor 1 atrás, Motor 2 adelante)
+        board.digital_write(in1, 0)
+        board.digital_write(in2, 1)
+        board.digital_write(in3, 1)
+        board.digital_write(in4, 0)
+        time.sleep(2)
         
         # Apagar LED
         board.digital_write(led_pin, 0)
         
-        # Detener motor
+        # Detener motores
         board.digital_write(in1, 0)
         board.digital_write(in2, 0)
+        board.digital_write(in3, 0)
+        board.digital_write(in4, 0)
         time.sleep(2)
 
 except KeyboardInterrupt:
